aa07572defcda102731eddce09ed6a438edf108a
FromFire <yuanxiasummer@icloud.com>
2020-10-26 23:10:17 +0800

    添加了Player、Token类；xml格式改为json；读取json尚有bug

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,17 +2,84 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour
-{
+// 地图类
+// 加载地图、管理地图状态
+public class Board : MonoBehaviour{
+
+    BoardEntity boardEntity;
+    
+    //地图名称
+    string mapName;
+
+    //玩家数目
+    public int playerNumbers{get;} 
+
+    //各方棋子的数量
+    private List<int> tokenNumbers{get;} 
+    
+    //地图
+    private int[] map; 
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        //读取地图json文件
+        string filename = "MapSample";
+        loadMapFromJson(filename);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
+
+    //从json文件中读取地图
+    void loadMapFromJson(string filename) {
+        Debug.Log("从json中加载地图");
+
+        //读取文件
+        string json = "";
+        TextAsset text = Resources.Load<TextAsset>(filename);
+        json = text.text;
+        Debug.Log(json);
+        Debug.Assert(!string.IsNullOrEmpty(json));
+
+        //转换json
+        Debug.Assert(boardEntity == null);
+        boardEntity = JsonUtility.FromJson<BoardEntity>(json);
+        Debug.Assert(boardEntity != null);
+
+        //控制台输出信息
+        Debug.Log(boardEntity.num);
+        Debug.Log("名称："+boardEntity.mapName);
+        Debug.Assert(boardEntity.players != null);
+        Debug.Log("玩家数目："+boardEntity.players.number);
+        for(int i=0; i<boardEntity.players.number;i++) {
+            Debug.Log("棋子数目"+i+":"+boardEntity.tokens[i].number);
+        }
+    }
+
+}
+
+[System.Serializable]
+public class PlayersEntity
+{
+    public int number { get; set; }
+}
+
+[System.Serializable]
+public class TokensEntity
+{
+    public int number { get; set; }
+}
+
+[System.Serializable]
+public class BoardEntity
+{
+    public int num{get;set;}
+
+    public string mapName { get; set; }
+
+    public PlayersEntity players { get; set; }
+
+    public List<TokensEntity> tokens { get; set; }
 }
--- Assets/Scripts/Rule.cs
+++ Assets/Scripts/Rule.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rule : MonoBehaviour
-{
+//规则类：管理棋子和棋盘/其他棋子的互动，包括高亮可到达格子、吃子判断等等
+public class Rule : MonoBehaviour {
+
+    private Board board;
+    private List<Token> tokens;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //board = GameObject.Find("Board").GetComponent<Board>();
+        //int playerNumber = board.playerNumbers;
+
     }
 
     // Update is called once per frame
