e2b568ac139400aded063172ab7c0a507cbf6ade
FromFire <yuanxiasummer@icloud.com>
2020-11-24 23:20:45 +0800

    可以鼠标滚轮放缩地图了

---
+++ Assets/Scripts/MainCameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraControl : MonoBehaviour
+{
+    Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //获取主摄像机
+        mainCamera=GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //鼠标滚轮缩小
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if(mainCamera.orthographicSize <= 10)
+                mainCamera.orthographicSize += 1;
+        }
+        //鼠标滚轮放大
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if(mainCamera.orthographicSize > 1)
+                mainCamera.orthographicSize -= 1;
+        }
+    }
+}
