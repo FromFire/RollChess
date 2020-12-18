1a69233cc6d43ebf7aa4af1efec2c3223c57d999
FromFire <yuanxiasummer@icloud.com>
2020-12-18 17:14:39 +0800

    可以从主菜单滑动到选关界面了

--- Assets/Scripts/MenuGUI.cs
+++ Assets/Scripts/MenuGUI.cs
@@ -6,38 +6,81 @@ using UnityEngine.SceneManagement;
 
 public class MenuGUI : MonoBehaviour
 {
+    string path = "Canvas/ScrollView/Viewport/Content";
 
-    //开始游戏按钮
-    Button startGameButton;
+    ScrollRect scrollRect;
+
+    //切换到选关页面按钮
+    Button chooseLevelButton;
+    //返回主菜单按钮
+    Button returnMainMenuButton;
     
     //打开关卡编辑器按钮
     Button startMapEditButton;
 
+    //是否需要滑动页面
+    bool needSlide = false;
+    //每次滑动的距离
+    float slideDistanceEachFrame;
+    //滑动所需帧数
+    int slideRestFrames;
+
     // Start is called before the first frame update
     void Start()
     {
-        //初始化开始游戏按钮
-        startGameButton = GameObject.Find("/GUI/StartGameButton").GetComponent<Button> ();
-		startGameButton.onClick.AddListener(StartGame);
+        //初始化scrollView
+        scrollRect = GameObject.Find("Canvas/ScrollView").GetComponent<ScrollRect> ();
+
+        //初始化界面切换按钮
+        chooseLevelButton = GameObject.Find(path+"/MainMenu/ChooseLevelButton").GetComponent<Button> ();
+		chooseLevelButton.onClick.AddListener(ToChooseLevel);
+        returnMainMenuButton = GameObject.Find(path+"/LevelChoose/ReturnMainMenuButton").GetComponent<Button> ();
+        returnMainMenuButton.onClick.AddListener(ToMainMenu);
 
         //初始化打开关卡编辑器按钮
-        startMapEditButton = GameObject.Find("/GUI/StartMapEditButton").GetComponent<Button> ();
+        startMapEditButton = GameObject.Find(path+"/MainMenu/StartMapEditButton").GetComponent<Button> ();
 		startMapEditButton.onClick.AddListener(StartMapEdit);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //滑动效果
+        if(needSlide) {
+            SlidePerFrame();
+        }
     }
 
-    //开始游戏，是startGameButton的点击相应函数
-    public void StartGame() {
-        SceneManager.LoadScene("ChessPlay");
+    //滑动到选关界面，是chooseLevelButton的点击响应函数
+    public void ToChooseLevel() {
+        SlideTo(1f);
     }
 
-    //开始关卡编辑器，是startMapEditButton的点击相应函数
+    //滑动到主菜单，是returnMainMenuButton的点击响应函数
+    public void ToMainMenu() {
+        SlideTo(0f);
+    }
+
+    //开始关卡编辑器，是startMapEditButton的点击响应函数
     public void StartMapEdit() {
         SceneManager.LoadScene("MapEdit");
     }
+
+    //ScrollView滑动到指定位置
+    public void SlideTo(float position) {
+        needSlide = true;
+        slideRestFrames = 60;
+        slideDistanceEachFrame = (position - scrollRect.horizontalNormalizedPosition) / slideRestFrames;
+    }
+
+    //滑动时每帧的处理函数
+    public void SlidePerFrame() {
+        if(slideRestFrames >= 1) {
+            slideRestFrames -= 1;
+            scrollRect.horizontalNormalizedPosition += slideDistanceEachFrame;
+        } else {
+            needSlide = false;
+        }
+    }
+
 }
