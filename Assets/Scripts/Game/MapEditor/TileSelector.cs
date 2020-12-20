863025f79c75533c2e02c79cdef95684dc02b563
iphelf <2871618714@qq.com>
2020-12-20 17:04:17 +0800

    从MapEditor中抽出TileSelector，用于直观地遍历Tile。

--- Assets/Scripts/Game/MapEditor/TileSelector.cs
+++ Assets/Scripts/Game/MapEditor/TileSelector.cs
@@ -1,19 +1,44 @@
-﻿using Structure;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Structure;
 using UnityEngine;
 
 namespace Game.MapEditor {
     /// <summary>
     ///   <para>Tile切换装置。</para>
     /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class TileSelector : MonoBehaviour {
-        // Start is called before the first frame update
-        void Start() { }
+        private List<TileType> selectedTileTypes = new List<TileType>() {
+            TileType.Land_Lawn_Green,
+            TileType.Special_Portal,
+            TileType.Token_Tank_Red
+        };
 
-        // Update is called once per frame
-        void Update() { }
+        private TileType selectedTileType {
+            get => selectedTileTypes[selectedTilemapTypeId];
+            set => selectedTileTypes[selectedTilemapTypeId] = value;
+        }
+
+        private TilemapType selectedTilemapType = TilemapType.Land;
+
+        private int selectedTilemapTypeId => MyTypes.GetId(selectedTilemapType) - 1;
 
         public TileType GetSelectedTileType() {
-            return new TileType();
+            return selectedTileType;
+        }
+
+        public void ShiftSelectedTileType(int offset = 1) {
+            MyTypes.Shift(selectedTileType, offset);
+        }
+
+        public TilemapType GetSelectedTilemapType() {
+            return selectedTilemapType;
+        }
+
+        public void ShiftSelectedTilemapType(int offset = 1) {
+            MyTypes.Shift(selectedTilemapType, offset);
         }
     }
 }
