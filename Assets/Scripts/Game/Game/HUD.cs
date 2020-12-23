19754c1c7062fd2111d160652d42255f59811b22
FromFire <yuanxiasummer@icloud.com>
2020-12-23 18:00:33 +0800

    胜利界面可以显示赢家颜色了；优化HUD代码和注释

--- Assets/Scripts/Game/Game/HUD.cs
+++ Assets/Scripts/Game/Game/HUD.cs
@@ -26,9 +26,6 @@ public class HUD : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //初始化roll点相关
-		rollButton.onClick.AddListener(RollDice);
-
         //初始化获胜界面
         gameReview.gameObject.SetActive(false);
     }
@@ -39,54 +36,66 @@ public class HUD : MonoBehaviour
         
     }
 
-    //掷骰子
-    //是RollButton的OnClick函数
-    public void RollDice() {
-        rule.RollDice();
-    }
-
-    //显示roll点按钮，隐藏步数
+    /// <summary>
+    ///   <para> 显示roll点按钮，隐藏步数 </para>
+    /// </summary>
     public void ShowRollButton() {
         rollButton.gameObject.SetActive(true);
         rollNumber.text = "";
     }
 
-    //隐藏roll点按钮，显示步数
+    /// <summary>
+    ///   <para> 隐藏roll点按钮，显示步数 </para>
+    /// </summary>
     public void ShowRollStep(int step) {
         rollButton.gameObject.SetActive(false);
         rollNumber.text = step + "步";
     }
 
-    //更新回合数
+    /// <summary>
+    ///   <para> 更新回合数 </para>
+    /// </summary>
     public void UpdateTurn(int turn) {
         turnCount.text = "第 " + turn + " 回合";
     }
 
-    //更新正在行动提示
+    /// <summary>
+    ///   <para> 更新正在行动提示 </para>
+    /// </summary>
     public void UpdateActionPlayer(int player) {
-        string str = "";
-        switch((PlayerColor)player) {
-            case PlayerColor.Red:
-                str += "<color=#DD0000FF>红棋</color>";
-                break;
-            case PlayerColor.Blue:
-                str += "<color=#0000DDFF>蓝棋</color>";
-                break;
-            case PlayerColor.Yellow:
-                str += "<color=#FFDD00FF>黄棋</color>";
-                break;
-            case PlayerColor.Green:
-                str += "<color=#00DD00FF>绿棋</color>";
-                break;
-        }
+        string str = GetColorfulTokenString((PlayerColor)player);
         str += "行动中";
         actionPlayer.text = str;
     }
 
-    //弹出获胜消息
+    /// <summary>
+    ///   <para> 获得着色的棋子文本 </para>
+    /// </summary>
+    string GetColorfulTokenString(PlayerColor color) {
+        string str = "";
+        switch((PlayerColor)color) {
+            case PlayerColor.Red:
+                str = "<color=#DD0000FF>红棋</color>";
+                break;
+            case PlayerColor.Blue:
+                str = "<color=#0000DDFF>蓝棋</color>";
+                break;
+            case PlayerColor.Yellow:
+                str = "<color=#FFDD00FF>黄棋</color>";
+                break;
+            case PlayerColor.Green:
+                str = "<color=#00DD00FF>绿棋</color>";
+                break;
+        }
+        return str;
+    }
+
+    /// <summary>
+    ///   <para> 弹出获胜消息 </para>
+    /// </summary>
     public void ShowGameReview(int winner) {
         gameReview.gameObject.SetActive(true);
         Text winnerInfo = GameObject.Find("/HUD/GameReview/WinnerInfo").GetComponent<Text> ();
-        winnerInfo.text = "玩家 " + winner + " 获胜了！";
+        winnerInfo.text = GetColorfulTokenString((PlayerColor)winner) + " 获胜了！";
     }
 }
