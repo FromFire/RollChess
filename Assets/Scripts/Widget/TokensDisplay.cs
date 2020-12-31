54b72be722322d43e5082c8780315d9f2ef24c1b
iphelf <2871618714@qq.com>
2020-12-31 21:56:08 +0800

    删除了TileType和TilemapType，其他相关的类全部恢复到编号为520a874的commit之前

--- Assets/Scripts/Widget/TokensDisplay.cs
+++ Assets/Scripts/Widget/TokensDisplay.cs
@@ -1,35 +1,74 @@
 ﻿using System.Collections.Generic;
 using Structure;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace Widget {
     public class TokensDisplay : MonoBehaviour {
         //Tilemap棋子层
-        public TilemapManager tilemapManager;
+        public Tilemap tilemapToken;
 
-        //player的值对应tokenList的下标
-        public List<TileType> tokenList;
+        //存储各类Tile的集合
+        //player的值对应tileList的下标
+        List<Tile> tileList;
+
+        //Tile在tileList中存储的顺序
+        enum TileKeys {
+            tokenRedAlien,
+            tokenBlueAlien,
+            tokenYellowAlien,
+            tokenGreenAlien,
+            tokenNeutralAlien
+        }; //棋子
+
+        // Start is called before the first frame update
+        void Start() {
+            // tile顺序按照enum tileKeys中规定的来
+            List<string> tileNames = new List<string> {
+                "Tiles/token-redAlien", //tokenRedAlien
+                "Tiles/token-blueAlien", //tokenBlueAlien
+                "Tiles/token-yellowAlien", //tokenYellowAlien
+                "Tiles/token-greenAlien", //tokenGreenAlien
+                "Tiles/token-neutralAlien" //tokenNeutralAlien
+            };
+
+            // 读取所有tile
+            tileList = new List<Tile>();
+            foreach (string name in tileNames) {
+                tileList.Add(Resources.Load<Tile>(name));
+            }
+        }
 
         //初始化显示
         public void Display(List<TokenEntity> entity) {
             //不同阵营棋子外观不同
+            List<Tile> tokenTiles = new List<Tile>();
+            tokenTiles.Add(tileList[(int) TileKeys.tokenRedAlien]);
+            tokenTiles.Add(tileList[(int) TileKeys.tokenBlueAlien]);
+            tokenTiles.Add(tileList[(int) TileKeys.tokenYellowAlien]);
+            tokenTiles.Add(tileList[(int) TileKeys.tokenGreenAlien]);
+            tokenTiles.Add(tileList[(int) TileKeys.tokenNeutralAlien]);
+
             //分阵营显示棋子
             foreach (TokenEntity token in entity) {
-                tilemapManager.SetTile(new Vector2Int(token.x, token.y), tokenList[token.player]);
+                tilemapToken.SetTile(new Vector3Int(token.x, token.y, 0), tokenTiles[token.player]);
             }
         }
 
         //在pos处显示number个player方的棋子
         //认定player为TileList的下标
         public void ShowToken(Vector2Int pos, int number, int player) {
+            //转换格式
+            Vector3Int pos3 = new Vector3Int(pos.x, pos.y, 0);
             //根据player获取棋子外观
-            TileType tile = tokenList[player];
+            Tile tile = tileList[player];
+
             //显示（目前不显示number）
             if (number == 0) {
-                tilemapManager.EraseTile(pos);
+                tilemapToken.SetTile(pos3, null);
             }
             else {
-                tilemapManager.SetTile(pos, tile);
+                tilemapToken.SetTile(pos3, tile);
             }
         }
 
