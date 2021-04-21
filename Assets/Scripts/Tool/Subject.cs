0e10445eb77eeacb7d0cd2edfd8c0cedc3331b23
FromFire <yuanxiasummer@icloud.com>
2021-04-21 17:11:28 +0800

    添加观察者模式

---
+++ Assets/Scripts/Tool/Subject.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///   <para> 更新订阅 </para>
+///   <para> Model有修改时，通过此类通知已注册的所有观察者 </para>
+/// </summary>
+public class Subject {
+
+    /// <summary>
+    ///   <para> 观察者，即发生修改时调用的函数 </para>
+    /// </summary>
+    public delegate void Observer();
+
+    /// <summary>
+    ///   <para> 观察者列表 </para>
+    /// </summary>
+    private Dictionary<ModelModifyEvent, List<Observer>> observers;
+
+    public Subject() {
+        observers = new Dictionary<ModelModifyEvent, List<Observer>>();
+    }
+
+    /// <summary>
+    ///   <para> 添加观察者 </para>
+    /// </summary>
+    public void Attach(ModelModifyEvent modelModifyEvent, Observer observer) {
+        // 合法性检查：事件不存在、数组为null
+        // 为该事件添加新数组
+        if(!observers.ContainsKey(modelModifyEvent) || observers[modelModifyEvent] == null) {
+            observers[modelModifyEvent] = new List<Observer>();
+        }
+
+        // 添加
+        observers[modelModifyEvent].Add(observer);
+    }
+
+    /// <summary>
+    ///   <para> 移除观察者 </para>
+    /// </summary>
+    public void Detach(ModelModifyEvent modelModifyEvent, Observer observer) {
+        // 合法性检查：事件不存在、数组为null
+        // List.Remove在对象不存在时返回false
+        // 直接返回
+        if(!observers.ContainsKey(modelModifyEvent) || observers[modelModifyEvent] == null) {
+            return;
+        }
+
+        // 移除
+        observers[modelModifyEvent].Remove(observer);
+    }
+
+    /// <summary>
+    ///   <para> 推送某一事件的修改 </para>
+    /// </summary>
+    public void Notify(ModelModifyEvent modelModifyEvent) {
+        // 合法性检查：事件不存在、数组为null
+        // 直接返回
+        if(!observers.ContainsKey(modelModifyEvent) || observers[modelModifyEvent] == null)
+            return;
+
+        // 推送
+        foreach(Observer observer in observers[modelModifyEvent]) {
+            observer();
+        }
+    }
+}
+
+/// <summary>
+///   <para> 更新订阅，继承于Subject </para>
+///   <para> Model有修改时，通过此类通知已注册的所有观察者 </para>
+///   <para> 同时通知被修改的坐标 </para>
+/// </summary>
+public class PositionSubject : Subject {
+
+    /// <summary>
+    ///   <para> 观察者，即发生修改时调用的函数 </para>
+    /// </summary>
+    public delegate void PositionObserver(Vector2Int position);
+
+    /// <summary>
+    ///   <para> 观察者列表 </para>
+    /// </summary>
+    private Dictionary<ModelModifyEvent, List<PositionObserver>> observers;
+
+    /// <summary>
+    ///   <para> 添加观察者 </para>
+    /// </summary>
+    public void Attach(ModelModifyEvent modelModifyEvent, PositionObserver observer) {
+        // 合法性检查：事件不存在、数组为null
+        // 为该事件添加新数组
+        if(!observers.ContainsKey(modelModifyEvent) || observers[modelModifyEvent] == null) {
+            observers[modelModifyEvent] = new List<PositionObserver>();
+        }
+
+        // 添加
+        observers[modelModifyEvent].Add(observer);
+    }
+
+    /// <summary>
+    ///   <para> 移除观察者 </para>
+    /// </summary>
+    public void Detach(ModelModifyEvent modelModifyEvent, PositionObserver observer) {
+        // 合法性检查：事件不存在、数组为null
+        // List.Remove在对象不存在时返回false
+        // 直接返回
+        if(!observers.ContainsKey(modelModifyEvent) || observers[modelModifyEvent] == null) {
+            return;
+        }
+
+        // 移除
+        observers[modelModifyEvent].Remove(observer);
+    }
+
+    /// <summary>
+    ///   <para> 推送某一事件的修改 </para>
+    /// </summary>
+    public void Notify(ModelModifyEvent modelModifyEvent, Vector2Int position) {
+        // 合法性检查：事件不存在、数组为null
+        // 直接返回
+        if(!observers.ContainsKey(modelModifyEvent) || observers[modelModifyEvent] == null)
+            return;
+
+        // 推送
+        foreach(PositionObserver observer in observers[modelModifyEvent]) {
+            observer(position);
+        }
+    }
+}
