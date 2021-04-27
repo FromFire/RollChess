877bb6e96d5d91bb7e91db306d480ce5f366ceb2
FromFire <yuanxiasummer@icloud.com>
2021-04-27 20:33:20 +0800

    完成HexGridDisplay

---
+++ Assets/Scripts/Prefabs/Game/HexGridDisplay.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/// <summary>
+///   <para> 显示地图的六边形网格 </para>
+///   <para> 比地图边界大一圈（20格） </para>
+/// </summary>
+public class HexGridDisplay : MonoBehaviour
+{
+    // 显示网格的Tilemap
+    [SerializeField] private TilemapManager tilemap;
+
+    // 是否显示网格
+    private bool visible;
+
+    // 已填充的地图边界
+    int borderUp, borderDown, borderLeft, borderRight;
+
+    // 显示格子
+    void ShowGrid() {
+        // 记录填充的边界，余量20
+        Board board = PublicResource.board;
+        borderUp = board.BorderUp + 20;
+        borderDown = board.BorderDown - 20;
+        borderLeft = board.BorderLeft - 20;
+        borderRight = board.BorderRight + 20;
+
+        // 填充
+        for(int i=borderLeft; i<borderRight; i++) {
+            for(int j=borderDown; j<borderUp; j++) {
+                tilemap.SetTile(new Vector2Int(i,j), TileType.HexGrid);
+            }
+        }
+    }
+
+    // 隐藏格子
+    void HideGrid() {
+        // 填充
+        for(int i=borderLeft; i<borderRight; i++) {
+            for(int j=borderDown; j<borderUp; j++) {
+                tilemap.RemoveTile(new Vector2Int(i,j));
+            }
+        }
+    }
+
+    /// <summary>
+    ///   <para> 是否显示网格 </para>
+    /// </summary>
+    public bool Visible {
+        get { return visible; }
+        set {
+            if(value) ShowGrid();
+            else HideGrid();
+            visible = value;
+        }
+    }
+}
