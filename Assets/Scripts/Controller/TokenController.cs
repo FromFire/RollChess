1c42b877e3caba761862b5b19be21a2c71c03034
FromFire <yuanxiasummer@icloud.com>
2021-04-28 17:12:41 +0800

    完成TokenController

--- Assets/Scripts/Controller/TokenController.cs
+++ Assets/Scripts/Controller/TokenController.cs
@@ -11,6 +11,14 @@ public class TokenController {
     ///   <para> 移动棋子，处理吃子 </para>
     /// </summary>
     public void Move(Vector2Int from, Vector2Int to) {
+        // 寻找from处的棋子
+        Dictionary<TokenSet.QueryParam, int> param = new Dictionary<TokenSet.QueryParam, int> {
+            {TokenSet.QueryParam.PositionX, from.x},
+            {TokenSet.QueryParam.PositionY, from.y}
+        };
+        int token = PublicResource.tokenSet.Query(param)[0];
 
+        // 移动
+        PublicResource.tokenSet.Move(token, to);
     }
 }
