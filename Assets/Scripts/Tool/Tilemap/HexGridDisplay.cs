042bacdfa0b027c36742ac6c8cf773d7102b7302
FromFire <yuanxiasummer@icloud.com>
2021-05-13 21:18:15 +0800

    网格和海洋的显示可以根据board的更新而随之延伸了

--- Assets/Scripts/Tool/Tilemap/HexGridDisplay.cs
+++ Assets/Scripts/Tool/Tilemap/HexGridDisplay.cs
@@ -15,25 +15,27 @@ public class HexGridDisplay : MonoBehaviour
     // 是否显示网格
     private bool visible;
 
-    // 已填充的地图边界
-    int borderUp, borderDown, borderLeft, borderRight;
+    // 已填充的边界
+    (int up, int down, int left, int right) border;
+
+    // 填充边界到地图边界的余量，默认20
+    int padding = 20;
 
     void Start() {
         ShowGrid();
+        // 注册更新
+        ModelResource.boardSubject.Attach(ModelModifyEvent.Cell, UpdateSelf);
     }
 
     // 显示格子
     void ShowGrid() {
         // 获取地图的边界，若没有边界（地图为空），则认为地图边界是(0,0)
         Board board = ModelResource.board;
-        borderUp = ((board.BorderUp == int.MinValue) ? 0 : board.BorderUp) + 20;
-        borderRight = ((board.BorderRight == int.MinValue) ? 0 : board.BorderRight) + 20;
-        borderLeft = ((board.BorderLeft == int.MaxValue) ? 0 : board.BorderLeft) - 20;
-        borderDown = ((board.BorderDown == int.MaxValue) ? 0 : board.BorderDown) - 20;
+        border = GetBoarder();
 
         // 绘制，边界余量20
-        for(int i=borderLeft; i<borderRight; i++) {
-            for(int j=borderDown; j<borderUp; j++) {
+        for(int i=border.left; i<=border.right; i++) {
+            for(int j=border.down; j<=border.up; j++) {
                 tilemap.SetTile(new Vector2Int(i,j), TileType.HexGrid);
             }
         }
@@ -42,11 +44,36 @@ public class HexGridDisplay : MonoBehaviour
     // 隐藏格子
     void HideGrid() {
         // 填充
-        for(int i=borderLeft; i<borderRight; i++) {
-            for(int j=borderDown; j<borderUp; j++) {
+        for(int i=border.left; i<=border.right; i++) {
+            for(int j=border.down; j<=border.up; j++) {
                 tilemap.RemoveTile(new Vector2Int(i,j));
             }
         }
+    }
+
+    /// <summary>
+    ///   <para> 响应Board更新</para>
+    /// </summary>
+    public void UpdateSelf(Vector2Int position) {
+        // 不可见时无视
+        if(!visible)
+            return;
+
+        // 更新边界
+        (int up, int down, int left, int right) newBorder = GetBoarder();
+        if(newBorder == border)
+            return;
+        ShowGrid();
+    }
+
+    // 获取应填充的边界
+    (int up, int down, int left, int right) GetBoarder() {
+        Board board = ModelResource.board;
+        int up = ((board.BorderUp == int.MinValue) ? 0 : board.BorderUp) + padding;
+        int right = ((board.BorderRight == int.MinValue) ? 0 : board.BorderRight) + padding;
+        int left = ((board.BorderLeft == int.MaxValue) ? 0 : board.BorderLeft) - padding;
+        int down = ((board.BorderDown == int.MaxValue) ? 0 : board.BorderDown) - padding;
+        return (up, down, left, right);
     }
 
     /// <summary>
--- Assets/Scripts/Tool/Tilemap/UnwalkableDisplay.cs
+++ Assets/Scripts/Tool/Tilemap/UnwalkableDisplay.cs
@@ -13,6 +13,12 @@ public class UnwalkableDisplay : MonoBehaviour
     // 由于海和陆地有相互遮挡关系，本类的tilemap和BoardDisplay中的tilemap是同一个
     [SerializeField] private TilemapManager tilemap;
 
+    // 已填充的边界
+    (int up, int down, int left, int right) border;
+
+    // 填充边界到地图边界的余量，默认20
+    int padding = 20;
+
     void Start() {
         Display();
         // 注册更新
@@ -25,14 +31,11 @@ public class UnwalkableDisplay : MonoBehaviour
     public void Display() {
         // 获取地图的边界，若没有边界（地图为空），则认为地图边界是(0,0)
         Board board = ModelResource.board;
-        int boardUp = (board.BorderUp == int.MinValue) ? 0 : board.BorderUp;
-        int boardRight = (board.BorderRight == int.MinValue) ? 0 : board.BorderRight;
-        int boardLeft = (board.BorderLeft == int.MaxValue) ? 0 : board.BorderLeft;
-        int borderDown = (board.BorderDown == int.MaxValue) ? 0 : board.BorderDown;
+        border = GetBoarder();
 
         // 绘制，边界余量20
-        for(int i=boardLeft - 20; i<boardRight + 20; i++) {
-            for(int j=borderDown - 20; j<boardUp + 20; j++) {
+        for(int i=border.left; i<=border.right; i++) {
+            for(int j=border.down; j<=border.up; j++) {
                 // 避免和陆地冲突
                 HashSet<Vector2Int> cellSet = tilemap.CellSet;
                 if(!cellSet.Contains(new Vector2Int(i,j)))
@@ -51,5 +54,31 @@ public class UnwalkableDisplay : MonoBehaviour
         if(!cell.Walkable && cell.Effect == SpecialEffect.None) {
             tilemap.SetTile(position, TileType.Ocean);
         }
+
+        // 更新边界
+        (int up, int down, int left, int right) newBorder = GetBoarder();
+        if(newBorder == border)
+            return;
+        // 边界扩大时，绘制随之扩大，缩小的不管
+        for(int i=newBorder.left; i<=newBorder.right; i++) {
+            for(int j=newBorder.down; j<=newBorder.up; j++) {
+                cell = ModelResource.board.Get(new Vector2Int(i, j));
+                // 如果在newBorder内而不在border内，则画之
+                // 和上面的不合并，因为不确定position是否在newBorder内
+                if( cell is null || (!cell.Walkable && cell.Effect == SpecialEffect.None) )
+                    tilemap.SetTile(new Vector2Int(i,j), TileType.Ocean);
+            }
+        }
+        border = newBorder;
+    }
+
+    // 获取应填充的边界
+    (int up, int down, int left, int right) GetBoarder() {
+        Board board = ModelResource.board;
+        int up = ((board.BorderUp == int.MinValue) ? 0 : board.BorderUp) + padding;
+        int right = ((board.BorderRight == int.MinValue) ? 0 : board.BorderRight) + padding;
+        int left = ((board.BorderLeft == int.MaxValue) ? 0 : board.BorderLeft) - padding;
+        int down = ((board.BorderDown == int.MaxValue) ? 0 : board.BorderDown) - padding;
+        return (up, down, left, right);
     }
 }
