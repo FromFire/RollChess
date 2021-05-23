3a0563e328b96b9035c7c7ff3509a002cf897b91
FromFire <yuanxiasummer@icloud.com>
2021-05-23 13:36:44 +0800

    单人线基本完成

--- Assets/Scripts/Scene/Entrance/Controller/EntranceController.cs
+++ Assets/Scripts/Scene/Entrance/Controller/EntranceController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 ///   <para> 地图选择中的一项 </para>
@@ -35,45 +36,49 @@ public class EntranceController : MonoBehaviour {
     ///   <para> 选择地图 </para>
     /// </summary>
     public void StartGame() {
-        // // 获取players
-        // List<PlayerChoices> player = choosePlayer.GetPlayers();
+        // 获取players
+        List<PlayerForm> player = new List<PlayerForm> {
+            EntranceResource.mapChooseState.GetPlayerForm(PlayerID.Red), 
+            EntranceResource.mapChooseState.GetPlayerForm(PlayerID.Blue), 
+            EntranceResource.mapChooseState.GetPlayerForm(PlayerID.Yellow), 
+            EntranceResource.mapChooseState.GetPlayerForm(PlayerID.Green)
+        };
 
-        // // 检查players的合法性
-        // // 4个player至少有1个由玩家操控，且player最少为2人
-        // int totalPlayers = 0;
-        // bool haveHuman = false;
-        // foreach(PlayerChoices choice in player) {
-        //     if(choice != PlayerChoices.Banned) {
-        //         totalPlayers ++;
-        //     }
-        //     if(choice == PlayerChoices.Player) {
-        //         haveHuman = true;
-        //     }
-        // }
-        // // 错误：若players人数低于最小人数限制
-        // if(totalPlayers < choosePlayer.MinPlayer) {
-        //     WarningManager.errors.Add(new WarningModel("角色数量最少为" + choosePlayer.MinPlayer + "人！"));
-        //     return;
-        // }
-        // // 错误：若玩家数为0
-        // if(!haveHuman) {
-        //     WarningManager.errors.Add(new WarningModel("至少有一个玩家参与游戏！"));
-        //     return;
-        // }
+        // 检查players的合法性
+        // 4个player至少有1个由玩家操控，且player最少为2人
+        int totalPlayers = 0;
+        bool haveHuman = false;
+        foreach(PlayerForm choice in player) {
+            if(choice != PlayerForm.Banned) {
+                totalPlayers ++;
+            }
+            if(choice == PlayerForm.Player) {
+                haveHuman = true;
+            }
+        }
+        // 错误：若players人数低于最小人数限制
+        int minPlayer = EntranceResource.mapChooseState.PlayerLimit.min;
+        if(totalPlayers < minPlayer) {
+            WarningManager.errors.Add(new WarningModel("角色数量最少为" + minPlayer + "人！"));
+            return;
+        }
+        // 错误：若玩家数为0
+        if(!haveHuman) {
+            WarningManager.errors.Add(new WarningModel("至少有一个玩家参与游戏！"));
+            return;
+        }
 
-        // //获取maps
-        // string mapName = chooseMap.mapName.text;
+        //获取maps
+        string mapName = EntranceResource.mapChooseState.MapFileName;
 
-        // //地图不能为空
-        // if(mapName.Length <= 1) {
-        //     WarningManager.errors.Add(new WarningModel("请选择地图！"));
-        //     return;
-        // }
+        //地图不能为空
+        if(mapName.Length <= 1) {
+            WarningManager.errors.Add(new WarningModel("请选择地图！"));
+            return;
+        }
 
-        // //符合要求，进入游戏
-        // GameObject.DontDestroyOnLoad(message.gameObject);
-        // message.SetMessage<string>("mapFilename", mapFilename);
-        // message.SetMessage<List<PlayerChoices>>("playerChoice", player);
-        // SceneManager.LoadScene("Game");
+        //符合要求，进入游戏
+        GameObject.DontDestroyOnLoad(EntranceResource.mapChooseState.gameObject);
+        SceneManager.LoadScene("Game");
     }
 }
