7b7ba6e01674a73a51dcd1e1482beb1c07cbac5e
FromFire <yuanxiasummer@icloud.com>
2021-05-25 14:53:11 +0800

    import mirror

---
+++ Assets/Scripts/Tool/Network/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+/// <summary>
+///   <para> 玩家 </para>
+/// </summary>
+public class Player : NetworkBehaviour {
+
+}
