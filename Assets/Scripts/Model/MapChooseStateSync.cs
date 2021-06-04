176fe086b3bccc2bfa446956d7a8b4b8c2342a17
FromFire <yuanxiasummer@icloud.com>
2021-06-04 16:32:59 +0800

    尝试同步thumb并失败了。不能用string，会超出容量

--- Assets/Scripts/Model/MapChooseStateSync.cs
+++ Assets/Scripts/Model/MapChooseStateSync.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 /// <summary>
 /// 用于同步Model
 /// </summary>
+// 同步步骤：
+// 1. Server修改Model，通过Subject推送给本类
+// 2. Subject响应函数UpdateSelf被调用，使本类成员变量与Model同步
+// 3. 本类成员变量被修改，通过SyncVar同步到Client
+// 4. 同步后，在Client上触发SyncVar的响应函数，修改对应flag
+// 5. flag满足条件后，触发SyncMapChooseState，修改Model，使Client上的Model与本类成员变量同步
 public class MapChooseStateSync : NetworkBehaviour {
-    private void Start() {
-        NetworkResource.networkSubject.Attach(ModelModifyEvent.Map_File_Name, UpdateSelf);
-        UpdateSelf();
-    }
-
     /// <summary>
     /// 选择地图，务必先同步本变量，再同步thumb和map
     /// </summary>
@@ -22,7 +23,7 @@ public class MapChooseStateSync : NetworkBehaviour {
     /// <summary>
     /// 地图略缩图
     /// </summary>
-    [SyncVar(hook = "SyncThumb")] public SyncList<byte> thumb;
+    public SyncList<byte> thumb = new SyncList<byte>();
     // 同步flag
     private bool thumbFlag = false;
     
@@ -33,6 +34,12 @@ public class MapChooseStateSync : NetworkBehaviour {
     // 同步flag
     private bool mapFlag = false;
     
+    private void Start() {
+        NetworkResource.networkSubject.Attach(ModelModifyEvent.Map_File_Name, UpdateSelf);
+        thumb.Callback += SyncThumb;
+        UpdateSelf();
+    }
+
     // mapChooseState修改时更新自身
     [Server]
     void UpdateSelf() {
@@ -45,13 +52,18 @@ public class MapChooseStateSync : NetworkBehaviour {
 
     // 将MapChooseState所需的信息同步。需要mapFileName，thumb和map都同步完成后再调用
     void SyncMapChooseState() {
+        Debug.Log("尝试同步");
+        Debug.Log(mapFileNameFlag);
+        Debug.Log(thumbFlag);
+        Debug.Log(thumb.Count);
+        Debug.Log(mapFlag);
         if (mapFileNameFlag && thumbFlag && mapFlag) {
             MapChooseState.Get().MapFileName = mapFileName;
             mapFileNameFlag = thumbFlag = mapFlag = false;
+            Debug.Log("同步地图：" + mapFileName);
         }
-        Debug.Log("同步地图：" + mapFileName);
     }
-    
+
     // 同步地图略缩图
     void SyncMapFileName(string oldValue, string newValue) {
         mapFileNameFlag = true;
@@ -59,9 +71,9 @@ public class MapChooseStateSync : NetworkBehaviour {
     }
     
     // 同步地图略缩图
-    void SyncThumb(SyncList<byte> oldValue, SyncList<byte> newValue) {
+    void SyncThumb(SyncList<byte>.Operation op, int i, byte b1, byte b2) {
         if (mapFileName is null) return;
-        SaveResource.saveManager.SaveThumb(newValue.ToArray(), mapFileName);
+        SaveResource.saveManager.SaveThumb(thumb.ToArray(), mapFileName);
         thumbFlag = true;
         SyncMapChooseState();
     }
@@ -70,7 +82,7 @@ public class MapChooseStateSync : NetworkBehaviour {
     void SyncMap(string oldValue, string newValue) {
         if (mapFileName is null) return;
         SaveResource.saveManager.SaveMap(SaveEntity.FromJson(newValue), mapFileName);
-        mapFileNameFlag = true;
+        mapFlag = true;
         SyncMapChooseState();
     }
 }
